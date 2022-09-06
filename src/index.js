1831a7b266df1c4dd6ac769fafede1de3c064577
mgomez05 <gomezruss7@gmail.com>
2022-09-06 16:17:47 -0400

    Removed all the extra code in index.js and replaced it with the code from the tutorial

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-// Import React, the ReactDom, and the CSS file
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-import App from './App';
-//import reportWebVitals from './reportWebVitals';
+class RussApp extends Component {
+  render() {
+    return (
+      <div className="RussApp">
+        <h1>Hello, React 2!</h1>
+      </div>
+    )
+  }
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+ReactDOM.render(<RussApp />, document.getElementById('root'))
