05f2eadaf9e554b2e69e1dd8df44e1413bce1eec
mgomez05 <gomezruss7@gmail.com>
2022-09-06 16:51:37 -0400

    Updated the RussApp component to display the TableAsSimpleComponent component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
-import Table from './Table'
+import TableAsSimpleComponent from './TableAsSimpleComponent'
 
 class RussApp extends Component {
   render() {
     return (
       <div className="container">
         <h1>Hello, React 2!</h1>
-        <Table />
+        <TableAsSimpleComponent />
       </div>
     )
   }
