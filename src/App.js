56b48dc8f0bd63739ce2094ed79feae44172af3a
mgomez05 <gomezruss7@gmail.com>
2022-09-06 16:44:01 -0400

    Updated the RussApp component to display a Table as well

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import Table from './Table'
 
 class RussApp extends Component {
   render() {
     return (
-      <div className="RussApp">
+      <div className="container">
         <h1>Hello, React 2!</h1>
+        <Table />
       </div>
     )
   }
