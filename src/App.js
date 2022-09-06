a11e2ecd9a7fd24cdd29c49443b3c2ec3ce7f14a
mgomez05 <gomezruss7@gmail.com>
2022-09-06 16:58:58 -0400

    Added a const characters to the App.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import TableThatUsesSimpleComponents from './TableThatUsesSimpleComponents'
 
 class RussApp extends Component {
   render() {
+
+    // The characters that will be displayed in the table
+    const CHARACTERS = [
+      {
+        name: 'Charlie',
+        job: 'Janitor'
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspiring'
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+
     return (
       <div className="container">
         <h1>Hello, React 2!</h1>
