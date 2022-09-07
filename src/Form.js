187cb64dc80656d723e61a149ff5d2e046c32574
mgomez05 <gomezruss7@gmail.com>
2022-09-07 18:50:52 -0400

    Added a render() to Form.js that returns a form with a name input and a job input

--- src/Form.js
+++ src/Form.js
@@ -17,4 +17,30 @@ class Form extends Component {
         })
     }
 
+    
+    render() {
+
+        const { name, job } = this.state
+
+        return (
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text"
+                       name="name"
+                       id="name"
+                       value={name}
+                       onChange={this.handleChange} />
+
+                <label htmlFor="job">Job</label>
+                <input type="text"
+                       name="job"
+                       id="job"
+                       value={job}
+                       onChange={this.handleChange} />
+
+            </form>
+        );
+    }
 }
+
+export default Form;
