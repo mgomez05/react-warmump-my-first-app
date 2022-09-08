5638f0266171351a61fded79d85c9b16e5c32725
mgomez05 <gomezruss7@gmail.com>
2022-09-08 00:08:55 -0400

    Updated index.js to use the new Api.js file for displaying the wikipedia api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import RussApp from './App'
+import RussApp from './Api'
 
 ReactDOM.render(<RussApp />, document.getElementById('root'))
